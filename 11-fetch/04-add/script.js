65a355e733d407bbb5a6406dfbfe2a0afed94733
René Modia <renelog.modia@gmail.com>
2021-10-21 10:47:09 +0200

    add last exercise

--- 11-fetch/04-add/script.js
+++ 11-fetch/04-add/script.js
@@ -11,37 +11,64 @@
 
 (() => {
     // your code here
-    let heroName = document.querySelector("#hero-name")
-    let alterego = document.querySelector("#hero-alter-ego")
-    let heroPower = document.querySelector("#hero-powers")
-    let firstLoad = false
-    let responseInJson
+    let heroName = document.querySelector("#hero-name");
+    let alterego = document.querySelector("#hero-alter-ego");
+    let heroPower = document.querySelector("#hero-powers");
+    let firstLoad = false;
+    let responseInJson;
+    var newID;
 
     document.querySelector("#run").addEventListener("click", async () => {
-        console.clear()
+        console.clear();
 
-        if (firstLoad === false){
+        if (heroName.value === "" || alterego.value === "" || heroPower.value === "") {
 
-            const response = await fetch("http://localhost:3000/heroes");
-            responseInJson = await response.json()
-            firstLoad=true
+            alert("Each field must be filled in");
 
+        } else {
+
+            let arrPower = heroPower.value.split(/\s*(?:,|$)\s*/);
+
+            try {
+                firstLoad = false;
+
+                async function refresh() {
+                    const response = await fetch("http://localhost:3000/heroes");
+                    responseInJson = await response.json();
+
+                    newID = responseInJson.length;
+                    
+                    if (firstLoad == true) {
+                        console.log(responseInJson);
+                    }
+                    
+                    firstLoad = true;
+                }
+                refresh();
+                newID +=1
+
+                const response = await fetch("http://localhost:3000/heroes", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        id: newID,
+                        name: heroName.value,
+                        alterEgo: alterego.value,
+                        abilities: arrPower,
+                    }),
+                });
+                //const data = await response.json();
+                refresh();
+                
+            } catch (error) {
+                console.error(error);
+            }
+
+            // responseInJson.push({id:newID, name:heroName.value, alterEgo:alterego.value, abilities : arrPower})
+            // console.log(responseInJson)
         }
-
-        let newID = responseInJson.length+1
-
-        if (heroName.value === "" || alterego.value === "" || heroPower.value  === ""){
-            alert("Each field must be filled in")
-        }else{
-            let arrPower =heroPower.value.split(/\s*(?:,|$)\s*/)
-            //console.log(arrPower);
-
-            responseInJson.push({id:newID, name:heroName.value, alterEgo:alterego.value, abilities : arrPower})
-            console.log(responseInJson)
-        }
-
-
-        
-    })
+    });
 
 })();
--- 11-fetch/05-delete/script.js
+++ 11-fetch/05-delete/script.js
@@ -14,17 +14,48 @@
     const inputID = document.querySelector("#hero-id")
 
     document.querySelector("#run").addEventListener("click", async () => {
-        const response = await fetch("http://localhost:3000/heroes");
-
+        console.clear()
+        
+        //let id = inputID.value
         let id = parseInt(inputID.value,10)
 
-        if (isNaN(id) || (id < 1) ){
+        if (isNaN(id) || (id < 10) ){
             alert("The id must be a whole number and not less than 1")
             inputID.value = 1
 
         } else{
 
             try {
+                function fetchdata(){
+                    
+                }
+                const response = await fetch("http://localhost:3000/heroes"+"/"+inputID.value, {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    /* body: JSON.stringify({
+                        id: inputID.value
+                    }), */
+                })
+                .then((response)=>{
+                   
+                    if (response.ok){
+                        console.log("ok")
+                    }else{
+                        console.log("ko")
+                    }
+                })
+                
+                const response2 = await fetch("http://localhost:3000/heroes");
+                const responseInJson = await response2.json();
+                console.log(responseInJson)
+                
+            } catch (error) {
+                console.error("Id not found ",error);
+            }
+
+            /* try {
                 let responseInJson = await response.json()
                 let copie_responseInJson = [...responseInJson]
 
@@ -43,10 +74,38 @@
 
             } catch (error) {
                 console.error(error)
-            }
+            } */
 
         }
         
     });
 
 })();
+
+/*
+const asyncPostCall = async () => {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                 method: 'POST',
+                 headers: {
+                   'Content-Type': 'application/json'
+                   },
+                   body: JSON.stringify({
+             // your expected POST request payload goes here
+                     title: "My post title",
+                     body: "My post content."
+                    })
+                 });
+                 const data = await response.json();
+              // enter you logic when the fetch is successful
+                 console.log(data);
+               } catch(error) {
+             // enter your logic for when there is an error (ex. error toast)
+
+                  console.log(error)
+                 } 
+            }
+
+asyncPostCall()
+
+*/
